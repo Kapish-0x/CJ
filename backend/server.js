6e66ab53087ea0a5143b65cab4bf83d418a60524
Kapish Rawat <kapishsinghrawat@gmail.com>
2026-06-14 17:50:23 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://codejudge-teal.vercel.app"
+    ],
     credentials: true,
   })
 );
